4fa62f74c0b1939c8f9a07c385f5b77d42bc76bd
gretkierewicz <piotr.gretkierewicz@gmail.com>
2020-12-06 21:37:03 +0100

    Minor refactoring

--- Load_Planning_Project/employees/views.py
+++ Load_Planning_Project/employees/views.py
@@ -1,20 +1,20 @@
-from rest_framework import viewsets
+from rest_framework.viewsets import ModelViewSet
 
 from .models import Degrees, Positions, Employees
 from .serializers import DegreeSerializer, PositionSerializer, EmployeeSerializer
 
 
-class DegreeViewSet(viewsets.ModelViewSet):
+class DegreeViewSet(ModelViewSet):
     queryset = Degrees.objects.all().order_by('name')
     serializer_class = DegreeSerializer
 
 
-class PositionViewSet(viewsets.ModelViewSet):
+class PositionViewSet(ModelViewSet):
     queryset = Positions.objects.all().order_by('name')
     serializer_class = PositionSerializer
 
 
-class EmployeeViewSet(viewsets.ModelViewSet):
+class EmployeeViewSet(ModelViewSet):
     queryset = Employees.objects.all().order_by('abbreviation')
     serializer_class = EmployeeSerializer
 
