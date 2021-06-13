2ea5b2359f652abbc9c748dff0b8be5930c357f6
gretkierewicz <piotr.gretkierewicz@gmail.com>
2021-06-13 11:24:01 +0200

    Refactoring generation of academic years

--- AGH/data/constants.py
+++ AGH/data/constants.py
@@ -5,16 +5,13 @@ DEPARTMENTS_LOWERCASE = 'wimir, weaiiib, wgig, wimiip, wggiis, wimic, wh, wwnig,
                   'wz, wo, wgtjz, wiwm'.split(', ')
 DEPARTMENTS_LOWERCASE.sort()
 
-# timedelta because academic year starts/ends with Oct (Oct as 10th month of the year - 2 months offset) * ~30.5 days
-DAY_OFFSET = (10 - 2) * 30.5
-# TODO: to be precise, at what month new academic year should be available to pick?
-ACADEMIC_TIME_WITH_OFFSET = datetime.now() - timedelta(days=-DAY_OFFSET)
+ACADEMIC_YEAR_ENDS_WITH = 2030
 ACADEMIC_YEAR_STARTS_WITH = 2015
 
 DEPARTMENTS = [(dep_name, dep_name.upper()) for dep_name in DEPARTMENTS_LOWERCASE]
 ACADEMIC_YEARS = [
-    f"{year}-{year + 1}" for year in range(ACADEMIC_YEAR_STARTS_WITH, ACADEMIC_TIME_WITH_OFFSET.year)
-][::-1]
+    f"{year}-{year + 1}" for year in range(ACADEMIC_YEAR_STARTS_WITH, ACADEMIC_YEAR_ENDS_WITH)
+]
 
 # pensum groups
 BADAWCZO_DYDAKTYCZNA = 'badawczo-dydaktyczna'
